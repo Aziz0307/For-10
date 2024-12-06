94c31d772518ad4ecdc92ff99b4e02c6c5b151f1
Aziz0307 <Aziz03072018@gmail.com>
2024-12-06 19:08:27 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace For10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n = Decimal.Parse(Console.ReadLine());
+            decimal S =0;
+            for (decimal i = 1; i <= n; i++)
+            {
+                S += 1 / i;
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
